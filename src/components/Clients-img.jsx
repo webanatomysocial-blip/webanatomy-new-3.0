546950c8957f138f6c4c4601f93ac3d8590eb4c8
Webanatomydev <webanatomy@Webanatomydevs-Mac-mini.local>
2026-06-24 15:08:00 +0530

    admin issues done

--- src/components/Clients-img.jsx
+++ src/components/Clients-img.jsx
@@ -5,16 +5,16 @@ import "@/css/Clients-img.css";
 import img1 from "@/assets/images/Clients-logos/1.png";
 import img2 from "@/assets/images/Clients-logos/2.png";
 import img3 from "@/assets/images/Clients-logos/3.png";
-import img4 from "@/assets/images/Clients-logos/4.png";
+import img4 from "@/assets/images/Clients-logos/new/global.png";
 import img5 from "@/assets/images/Clients-logos/5.png";
-import img6 from "@/assets/images/Clients-logos/6.png";
+import img6 from "@/assets/images/Clients-logos/new/police.png";
 import img7 from "@/assets/images/Clients-logos/7.png";
-import img8 from "@/assets/images/Clients-logos/8.png";
+import img8 from "@/assets/images/Clients-logos/new/scy.png";
 import img9 from "@/assets/images/Clients-logos/9.png";
-import img10 from "@/assets/images/Clients-logos/10.png";
+import img10 from "@/assets/images/Clients-logos/new/ze.png";
 import img11 from "@/assets/images/Clients-logos/11.png";
 import img12 from "@/assets/images/Clients-logos/12.png";
-import img13 from "@/assets/images/Clients-logos/13.png";
+import img13 from "@/assets/images/Clients-logos/new/bitflow.png";
 import img14 from "@/assets/images/Clients-logos/14.png";
 import img15 from "@/assets/images/Clients-logos/15.png";
 import img16 from "@/assets/images/Clients-logos/16.png";
@@ -22,7 +22,7 @@ import img17 from "@/assets/images/Clients-logos/17.png";
 import img18 from "@/assets/images/Clients-logos/18.png";
 import img19 from "@/assets/images/Clients-logos/19.png";
 import img20 from "@/assets/images/Clients-logos/20.png";
-import img21 from "@/assets/images/Clients-logos/21.png";
+import img21 from "@/assets/images/Clients-logos/new/cearmic.png";
 import img22 from "@/assets/images/Clients-logos/22.png";
 import img23 from "@/assets/images/Clients-logos/23.png";
 import img24 from "@/assets/images/Clients-logos/24.png";
@@ -33,17 +33,17 @@ import img28 from "@/assets/images/Clients-logos/28.png";
 import img29 from "@/assets/images/Clients-logos/29.png";
 import img30 from "@/assets/images/Clients-logos/30.png";
 import img31 from "@/assets/images/Clients-logos/31.png";
-import img32 from "@/assets/images/Clients-logos/32.png";
-import img33 from "@/assets/images/Clients-logos/33.png";
+import img32 from "@/assets/images/Clients-logos/new/damro.png";
+// import img33 from "@/assets/images/Clients-logos/33.png";
 import img34 from "@/assets/images/Clients-logos/34.png";
-import img35 from "@/assets/images/Clients-logos/35.png";
-import img36 from "@/assets/images/Clients-logos/36.png";
+import img35 from "@/assets/images/Clients-logos/new/desi.png";
+import img36 from "@/assets/images/Clients-logos/new/yash.png";
 import img37 from "@/assets/images/Clients-logos/37.png";
 import img38 from "@/assets/images/Clients-logos/38.png";
 import img39 from "@/assets/images/Clients-logos/39.png";
 import img40 from "@/assets/images/Clients-logos/40.png";
 import img41 from "@/assets/images/Clients-logos/41.png";
-import img42 from "@/assets/images/Clients-logos/42.png";
+import img42 from "@/assets/images/Clients-logos/new/lotus.png";
 import img43 from "@/assets/images/Clients-logos/43.png";
 import img44 from "@/assets/images/Clients-logos/44.png";
 import img45 from "@/assets/images/Clients-logos/45.png";
@@ -66,7 +66,7 @@ const clientLogos = [
   img5,
   img6,
   img7,
-  // img8,
+  img8,
   // img9,
   img10,
   img11,
@@ -91,10 +91,10 @@ const clientLogos = [
   img30,
   img31,
   img32,
-  img33,
+  // img33,
   img34,
   img35,
-  // img36,
+  img36,
   img37,
   img38,
   img39,
